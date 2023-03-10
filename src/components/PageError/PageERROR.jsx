040de6fc6e70fcda803463de206063f943c77caa
Edgar Arcos <edgararcosmartin@gmail.com>
2023-03-10 12:08:10 +0000

    Pagina error retocada

--- src/components/PageError/PageERROR.jsx
+++ src/components/PageError/PageERROR.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const PageERROR = () => {
   return (
-    <div>PageERROR</div>
+      <div> 
+    <h1>Error-404-Error</h1> 
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta deleniti unde fugiat autem necessitatibus, pariatur reprehenderit tempore id doloribus commodi exercitationem perferendis voluptatibus a quasi culpa aspernatur temporibus! Magnam, unde.</p>
+    </div>
   )
 }
